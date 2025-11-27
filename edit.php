36e1581c473bb4e0421b4128eba1d4af83c3382a
[Novae] <[tommyputrasuharmadi@gmail.com]>
2025-11-27 12:16:45 +0700

    Menambahkan fitur edit

--- edit.php
+++ edit.php
@@ -35,10 +35,44 @@ if ($SERVER['REQUEST_METHOD'] === 'POST'){
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Edit Data Motor</h1>
-    <form method="POST" action>
-    <label for="id">ID: </label>
-    <input type="text" value="<?= $tampil['id']?>" name="id" required>
-    </form>
+    <div class="contain">
+        <h1>Edit Data Motor</h1>
+        <form method="POST" action>
+            <div class="field">
+            <label for="id">ID: </label>
+            <input type="text" value="<?= $tampil['id']?>" name="id" required>
+            </div>
+
+            <div class="field">
+            <label for="nama">Nama: </label>
+            <input type="text" value="<?= $tampil['nama']?>" name="nama" required>
+            </div>
+
+            <div class="field">
+            <label for="telepon">No. Telepon</label>
+            <input type="int" value="<?= $tampil['telepon']?>" name="telepon" required>
+            </div>
+
+            <div class="field">
+            <label for="jenis_motor">Jenis Motor</label>
+            <select name="jenis_motor" value="<?=$tampil['jenis_motor']?>" id="jenis_motor" required>
+            <option value="">Honda</option>
+            <option value="">Beat</option>
+            </select>
+            </div>
+
+            <div class="field">
+                <label for="tahun_pembuatan">Tahun Pembuatan</label>
+                <input type="varchar" name="tahun_pembuatan" id="tahun_pembuatan">
+            </div>
+
+            <div class="field">
+                <label for="feedback">Keluhan</label>
+                <textarea name="feedback" id="feedback"></textarea>
+            </div>
+            
+            <button>Edit</button>
+        </form>
+    </div>
 </body>
 </html>
