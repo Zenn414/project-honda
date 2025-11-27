57721ea987ca474b6c914b8955e3b05990207677
[Novae] <[tommyputrasuharmadi@gmail.com]>
2025-11-27 11:29:41 +0700

    Menkoneksikan database kedalam form login

---
+++ login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<?php
+    include 'db.php';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contain">
+        <h1>Service Motor</h1>
+        <form action="">
+            <div class="field">
+                <label for="">Nama </label>
+                <input type="text" name="" id="">
+            </div>
+
+              <div class="field">
+                <label for="">No Telp </label>
+                <input type="text" name="" id="">
+            </div>
+
+              <div class="field">
+                <label for="">Jenis Motor </label>
+               <select name="" id="">
+                <option value="">Honda</option>
+                <option value="">Beat</option>
+               </select>
+                </div>
+               
+
+                <div class="field">
+                <label for="">Tahun Pembuatan </label>
+                <input type="text" name="" id="">
+                </div>
+
+                 <div class="field">
+                <label for="">Keluhan</label>
+                <textarea name="" id=""></textarea>
+                </div>
+
+                <button>Kirim</button>
+        </form>
+    </div>
+</body>
+</html>
