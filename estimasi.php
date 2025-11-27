8c25b969253bea5e5d0445120362ab411fdcb470
ammmar@gmail.com <Ammar>
2025-11-27 12:33:25 +0700

    style form login telah dibuat

---
+++ estimasi.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estimasi</title>
+</head>
+<body>
+    <div class="contain">
+        <div class="card">
+
+        <h1>Keterangan Service</h1>
+
+            <div class="output">
+                <div class="field">
+                    <p>Nama: </p>
+                    
+                </div>
+                
+                 <div class="field">
+                    <p>No Telp: </p>
+                </div>
+
+                 <div class="field">
+                    <p>Tipe Motor: </p>
+                </div>
+
+                 <div class="field">
+                    <p> Tahun Keluaran: </p>
+                </div>
+
+                 <div class="field">
+                    <p>Keluhan: </p>
+                </div>
+
+            </div>
+
+            <div class="date">
+                <p>21/2/2025</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
