bd78690cb203bce66d54b74f5e73c8ecca7a4d3a
[Novae] <[tommyputrasuharmadi@gmail.com]>
2025-11-27 12:03:30 +0700

    Mengkoneksikan Input data kedalam database

--- login.php
+++ login.php
@@ -8,26 +8,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Form Servis Motor</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <div class="contain">
         <h1>Service Motor</h1>
         <form action="">
             <div class="field">
-                <label for="">Nama </label>
+                <label for="nama">Nama </label>
                 <input type="text" name="nama" id="nama">
             </div>
 
               <div class="field">
-                <label for="">No Telp </label>
-                <input type="text" name="telepon" id="">
+                <label for="telepon">No Telp </label>
+                <input type="text" name="telepon" id="telepon">
             </div>
 
               <div class="field">
-                <label for="">Jenis Motor </label>
-               <select name="jenis_motor" id="">
+                <label for="jenis_motor">Jenis Motor </label>
+               <select name="jenis_motor" id="jenis_motor">
                 <option value="">Honda</option>
                 <option value="">Beat</option>
                </select>
@@ -35,13 +35,13 @@
                
 
                 <div class="field">
-                <label for="">Tahun Pembuatan </label>
-                <input type="text" name="" id="">
+                <label for="tahun_pembuatan">Tahun Pembuatan </label>
+                <input type="text" name="tahun_pembuatan" id="tahun_pembuatan">
                 </div>
 
                  <div class="field">
-                <label for="">Keluhan</label>
-                <textarea name="" id=""></textarea>
+                <label for="feedback">Keluhan</label>
+                <textarea name="feedback" id="feedback"></textarea>
                 </div>
 
                 <button>Kirim</button>
